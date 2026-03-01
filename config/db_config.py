fc86742286fd0d79e6892a578f9e49bb49730776
CristianArch <cristianaltamar0314@gmail.com>
2026-03-01 13:41:17 -0500

    Refactor database configuration to use environment variables and remove settings module

--- config/db_config.py
+++ config/db_config.py
@@ -1,12 +1,14 @@
 import psycopg2
-from app.config.settings import settings
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 def get_db_connection():
     return psycopg2.connect(
-        host=settings.database_host,
-        port=settings.database_port,
-        user=settings.database_user,
-        password=settings.database_password,
-        dbname=settings.database_name
+        host=os.getenv("DBHOST"),
+        port=os.getenv("DBPORT"),
+        user=os.getenv("DBUSER"),
+        password=os.getenv("DBPASSWORD"),
+        database=os.getenv("DBNAME")
     )
